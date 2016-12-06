621b85942ac1450e0209d926da436382374bae3a
Victor Carpenter <victorcarpenter123@gmail.com>
2016-12-06 14:17:20 -0500

    got the character moving

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,11 +10,18 @@ var material = new THREE.MeshBasicMaterial( { color: 0xBFBFBF } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+var charGeometry = new THREE.BoxGeometry(1, 1.2, 0.5);
+var charMaterial = new THREE.MeshBasicMaterial({ color: 0xA0A0A0 })
+var chara = new THREE.Mesh(charGeometry, charMaterial);
+scene.add(chara);
+chara.position.set(-4.7, -1.2, 0);
+
+
 var groundGeometry = new THREE.BoxGeometry(12,0.3,1);
 var groundMaterial = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
 var ground = new THREE.Mesh( groundGeometry, groundMaterial );
 scene.add( ground );
-ground.position.set(0, -1, 0);
+ground.position.set(0, -2, 0);
 var light = new THREE.PointLight( 0x000000 );
 light.position.set( 10, 0, 10 );
 scene.add( light );
@@ -30,13 +37,27 @@ var render = function () {
 	// cube.rotation.z += 0.0175;
 
 
-
+  window.addEventListener("keydown", function(event){
+    if (event.keyCode === 39){
+      chara.position.x += 0.0019
+      console.log(chara.position.x)
+      if(chara.position.x >= 5.272000000000166){
+        chara.position.x = 5.272000000000166;
+      }
+    }else if (event.keyCode === 37){
+      chara.position.x += -0.0019
+      console.log(chara.position.x)
+      if(chara.position.x <= -4.795999999999992){
+        chara.position.x = -4.795999999999992;
+      }
+    }
+})
 
 	var dtime	= Date.now()
 	// cube.scale.x	= 1.0 + 0.3*Math.sin(dtime/300);
 	// cube.scale.y	= 1.0 + 0.3*Math.sin(dtime/300);
 	// cube.scale.z	= 1.0 + 0.3*Math.sin(dtime/300);
-  cube.position.y = 0.8 + 1*Math.sin(dtime/300);
+  cube.position.y = -0.4 + 1*Math.sin(dtime/300);
   renderer.render(scene, camera);
 };
 
